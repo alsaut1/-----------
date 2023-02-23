b27a3df165c7bd40fabe86855581d5cd835794d8
alsaut1 <alsaut@seznam.cz>
2023-02-23 13:05:29 +0100

    1.01

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-alert ('Привет');
+function getRandomIntInclusive(min, max) {
+  if (min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+  return 'error';
+}
+
+function getRandomArbitrary(min, max,decimalPoint =0 ) {
+  if (max > min){
+    return (Math.random() * (max - min) + min).toFixed(decimalPoint);
+  }
+  return 'error';
+}
+
+console.log (getRandomArbitrary(5.3,10.23,5));
