db169ff99113d4dc0499689bfd2cc3c430fbd3e6
alsaut1 <alsaut@seznam.cz>
2023-02-23 13:51:22 +0100

    1.02

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 function getRandomIntInclusive(min, max) {
-  if (min < max) {
+  if ( min >0 && max >0) {
+    if (max <min) {
+      [min, max] = [max, min];
+    }
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
   }
-  return 'error';
+  return -1;
 }
 
 function getRandomArbitrary(min, max,decimalPoint =0 ) {
-  if (max > min){
+  if (min >0 && max >0){
+    if (max <min) {
+      [min, max] = [max, min];
+    }
     return (Math.random() * (max - min) + min).toFixed(decimalPoint);
   }
-  return 'error';
+  return -1;
 }
 
+console.log (getRandomIntInclusive(5.3,10.23,5));
 console.log (getRandomArbitrary(5.3,10.23,5));
