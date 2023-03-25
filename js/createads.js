0764380775f9221c8d8be55ced44f0bf0a6bf158
alsaut1 <alsaut@seznam.cz>
2023-03-25 18:01:09 +0100

    1.04

---
+++ js/createads.js
@@ -0,0 +1,5 @@
+import {simularData} from './data.js';
+let templAds = document.querySelector('#card').content;
+let ads = templAds.querySelector();
+//console.log(simularData);
+console.log(templAds);
